b59b1dd81753e9e84d09d822555660c94fd2380d
Alexey <murcast.zero@gmail.com>
2020-05-19 20:36:03 +0400

    added tests

--- src/test/java/com/epam/project/ApplicationTest.java
+++ src/test/java/com/epam/project/ApplicationTest.java
@@ -1,22 +1,31 @@
 package com.epam.project;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.IsNull.notNullValue;
 
-@SpringBootTest(classes = Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApplicationTest {
 
     @Autowired
-    private ConfigurableApplicationContext context;
+    private ApplicationContext context;
 
     @Test
     public void test() {
-//        assertThat(context, notNullValue());
+        assertThat(context, notNullValue());
     }
 
+    @Test
+    public void beanTest() {
+        Clazz clazz = context.getBean("clazz", Clazz.class);
+        assertThat(clazz, Matchers.instanceOf(Object.class));
+    }
 }
