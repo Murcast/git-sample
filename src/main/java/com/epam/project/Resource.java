e22ca030e56149d0ff419053f3dff3b93b26128b
Alexey <murcast.zero@gmail.com>
2020-05-24 12:14:04 +0400

    -refactored

--- src/main/java/com/epam/project/Resource.java
+++ src/main/java/com/epam/project/Resource.java
@@ -2,10 +2,12 @@ package com.epam.project;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 @RequestMapping(value = "/resource")
 public class Resource {
 
